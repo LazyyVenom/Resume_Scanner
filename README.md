c40c5db0a3eff4adf7ae292209aa4cf409d29e52
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-04-06 22:09:56 +0530

    Local NPM breaked

--- README.md
+++
@@ -1,2 +0,0 @@
-# Resume_Scanner
-Scan Karenge Bhai do chaar feature aur h 
--- backend/app/main.py
+++
@@ -1,8 +0,0 @@
-# filepath: c:\Users\Anubhav Choubey\Documents\College_Work\Resume_Scanner\backend\app\main.py
-
-from fastapi import FastAPI
-from .routes import get_routes
-
-app = FastAPI()
-
-app.include_router(get_routes())
